8cfde254def93c850b679be1af7aa8f7f30cdda1
Inga Feick <inga.feick@trivago.com>
2016-03-15 16:48:46 +0100

    Fix rspec

--- spec/filters/list2filters_spec.rb
+++ spec/filters/list2filters_spec.rb
@@ -43,7 +43,6 @@ describe LogStash::Filters::List2fields do
       end
 
       it "should remove the input field" do       
-        expect(event["foo"]).to eq("bar")
         expect(event["message"]).to be_empty
       end # it
     end # context
@@ -51,7 +50,7 @@ describe LogStash::Filters::List2fields do
     context "when remove_source is set to false" do
 
       let(:event) { LogStash::Event.new("message" => [{"key"=>"foo","value"=>"bar"}]) } 
-      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message") }
+      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message", "remove_source" => false) }
     
       before do
         plugin.register
@@ -59,28 +58,14 @@ describe LogStash::Filters::List2fields do
       end
 
       it "should not remove the input field" do       
-        expect(event["foo"]).to eq("bar")
         expect(event["message"]).not_to be_empty
       end # it
     end # context
 
 
-    context "when there is a nested list" do
-
-      let(:event) { LogStash::Event.new("message" => [{"key"=>"foo","value"=>"bar"},{"key"=>"cheese","value"=>"gorgonzola"}]) } 
-      before do
-        plugin.filter(event)
-      end
-
-      it "should have new fields" do       
-        expect(event["foo"]).to eq("bar")
-        expect(event["cheese"]).to eq("gorgonzola")
-      end # it
-    end # context  
-
-    context "operates on a list of hashes with splitted key and value entries (default names) (testcase 1)" do
-      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message") } 
+    context "operates on a list of hashes with splitted key and value entries (using names) (testcase 1)" do
       let(:event) { LogStash::Event.new("cheese" => "chili", "message" => [{"key"=>"foo","value"=>"bar"},{"key"=>"cheese","value"=>"gorgonzola"}]) } 
+      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message", "key" => "key", "value" => "value") }
       before do
         plugin.register
         plugin.filter(event)
@@ -92,22 +77,8 @@ describe LogStash::Filters::List2fields do
       end # it
     end # context
 
-    context "operates on a list of hashes with splitted key and value entries (custom names) (testcase 1)" do
-      let(:event) { LogStash::Event.new("metric" => "fan", "message" => [{"metric"=>"disc","measured"=>"20"},{"metric"=>"cpu","measured"=>"8"}]) } 
-      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message", "key" => "metric", "value" => "measured") } 
-      before do
-        plugin.register
-        plugin.filter(event)
-      end
-
-      it "should have new fields" do
-        expect(event["disc"]).to eq("20")
-        expect(event["cpu"]).to eq("8")
-      end # it
-    end # context
 
     context "operates on a list of hashes with key and value in one tuple (testcase 2)." do
-      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message") } 
       let(:event) { LogStash::Event.new("cheese" => "chili", "message" => [{"foo"=>"bar"},{"cheese"=>"gorgonzola"}]) } 
       before do
         plugin.register
@@ -124,8 +95,8 @@ describe LogStash::Filters::List2fields do
 
   context "with prefix " do
 
-    context "operates on a list of hashes with splitted key and value entries (default names) (testcase 1)" do
-      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message", "prefix" => "l2f_") } 
+    context "operates on a list of hashes with splitted key and value entries (using names) (testcase 1)" do
+      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message", "prefix" => "l2f_", "key" => "key", "value" => "value") } 
       let(:event) { LogStash::Event.new("cheese" => "chili", "message" => [{"key"=>"foo","value"=>"bar"},{"key"=>"cheese","value"=>"gorgonzola"}]) } 
       before do
         plugin.register
@@ -142,23 +113,7 @@ describe LogStash::Filters::List2fields do
       end # it
     end # context
 
-    context "operates on a list of hashes with splitted key and value entries (custom names) (testcase 1)" do
-      let(:event) { LogStash::Event.new("metric" => "fan", "message" => [{"metric"=>"disc","measured"=>"20"},{"metric"=>"cpu","measured"=>"8"}]) } 
-      let(:plugin) { LogStash::Filters::List2fields.new("source" => "message", "prefix" => "l2f_", "key" => "metric", "value" => "measured") } 
-      before do
-        plugin.register
-        plugin.filter(event)
-      end
 
-      it "should have new fields with the prefix in the key" do
-        expect(event["l2f_disc"]).to eq("20")
-        expect(event["l2f_cpu"]).to eq("8")
-      end # it
-
-      it "should not overwrite existing fields" do
-        expect(event["metric"]).to eq("fan")
-      end # it
-    end # context
 
     context "operates on a list of hashes with key and value in one tuple (testcase 2)." do
       let(:plugin) { LogStash::Filters::List2fields.new("source" => "message", "prefix" => "l2f_") } 
